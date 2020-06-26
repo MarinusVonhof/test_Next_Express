326475eb3d9c9feccb1f004fad5f5c733205034f
marinus vonhof <marinus.vonhof@live.nl>
2020-06-26 13:37:07 +0200

    div

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "0620b7c58e0f6f6643d1dd663a87864c"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "ec42361e1e04cb3fd71cffbcd5602161"
+    "revision": "90d2a489a232e5db0183e19d8861b5a4"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
